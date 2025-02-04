5a8febce4909a460f6b8793cb1193567b11e83f7
171201 <jinalshah5358@gmail.com>
2025-02-04 23:19:36 +0530

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,7 +142,6 @@ export default function Page() {
                   key={project.title}
                   title={project.title}
                   description={project.description}
-                  dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
                   video={project.video}
@@ -174,25 +173,6 @@ export default function Page() {
                 </p>
               </div>
             </div>
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 14}>
-            <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
-              {DATA.hackathons.map((project, id) => (
-                <BlurFade
-                  key={project.title + project.dates}
-                  delay={BLUR_FADE_DELAY * 15 + id * 0.05}
-                >
-                  <HackathonCard
-                    title={project.title}
-                    description={project.description}
-                    location={project.location}
-                    dates={project.dates}
-                    image={project.image}
-                    links={project.links}
-                  />
-                </BlurFade>
-              ))}
-            </ul>
           </BlurFade>
         </div>
       </section>
